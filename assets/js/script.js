3a7ba05dd096bf49fb4cb44133266b4724a54ea1
Sankalp <sanks@Pratibhas-MacBook-Pro.local>
2016-11-01 10:17:27 +0530

    Got the code working with vue 2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,28 +3,60 @@
 -----------------*/
 
 // Parent | Subreddit component containing a list of 'post' components. 
-var subreddit = Vue.component('subreddit',{
+Vue.component( 'subreddit', {
+	
+	props: ['subname'],
 	template: '#subreddit',
-	props: ['name'],
-
 	data: function () {
 		return { posts: [] }
 	},
-
+	computed: {
+    	uppername: function () {
+    	  	return this.subname.toUpperCase()
+    	}
+  	},
 	created: function(){
-	    this.$http.get("https://www.reddit.com/r/"+ this.name +"/top.json?limit=5")
+	    this.$http.get("https://www.reddit.com/r/"+ this.subname +"/top.json?limit=5")
 	    .then(function(resp){
 	        this.posts=resp.data.data.children;
 	    });
 	}
-});
+})
 
 
 // Child | Componenet represiting a single post.
-var post = Vue.component('post', {
+Vue.component('post', {
 	template: "#post",
-	props: ['item']
-});
+	props: ['item'],
+	methods: {
+		// Filter that takes an image url and creates a CSS style.
+		setAsBackground: function(value) {
+			if(value && value!='self') {
+				return 'background-image: url(' + value + ')';	
+			}
+			else {
+				return 'background-image: url(assets/img/placeholder.png)';	
+			}
+		},
+		// Removes the &amp; bug that happens in urls returned from Reddit image uploads
+		cleanUrl: function(badurl) {
+			return badurl.replace(/&amp;/g, '&')
+		}
+	},
+	filters: {
+		// Filter for cutting off strings that are too long.
+		truncate: function(value) {
+			var length = 60;
+
+			if(value.length <= length) {
+				return value;
+			}
+			else {
+				return value.substring(0, length) + '...';			
+			}
+		}
+	},
+})
 
 
 
@@ -32,29 +64,6 @@ var post = Vue.component('post', {
 /*-----------------
    Custom filters 
 -----------------*/
-
-// Filter for cutting off strings that are too long.
-Vue.filter('truncate', function(value) {
-	var length = 60;
-
-	if(value.length <= length) {
-		return value;
-	}
-	else {
-		return value.substring(0, length) + '...';			
-	}
-});
-
-
-// Filter that takes an image url and creates a CSS style.
-Vue.filter('setAsBackground', function(value) {
-	if(value && value!='self' && value!='nsfw') {
-		return 'background-image: url(' + value + ')';	
-	}
-	else {
-		return 'background-image: url(assets/img/placeholder.png)';	
-	}
-});
 
 
 
@@ -65,4 +74,4 @@ Vue.filter('setAsBackground', function(value) {
 
 new Vue({
 	el: '#vueContainer'
-});
+})
